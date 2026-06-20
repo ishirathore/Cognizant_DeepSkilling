ece45e3bdeeb63ac089532ea5efb266723a2d5ca
Ishika Rathore <ishikarathore030@gmai.com>
2026-06-20 17:54:01 +0530

    Added SingletonPatternExample

---
+++ Week_1/SingletonPatternExample/src/Logger.java
@@ -0,0 +1,23 @@
+//package Week_1.SingletonPatternExample.src;
+public class Logger {
+    // Private static instance
+    private static Logger instance;
+
+    // Private constructor
+    private Logger() {
+        System.out.println("Logger initialized...");
+    }
+
+    // Public static method to get the instance
+    public static Logger getInstance() {
+        if (instance == null) {
+            instance = new Logger();
+        }
+        return instance;
+    }
+
+    // Example logging method
+    public void log(String message) {
+        System.out.println("[LOG]: " + message);
+    }
+}
---
+++ Week_1/SingletonPatternExample/src/SingletonTest.java
@@ -0,0 +1,12 @@
+//package Week_1.SingletonPatternExample.src;
+public class SingletonTest {
+    public static void main(String[] args) {
+        Logger logger1 = Logger.getInstance();
+        Logger logger2 = Logger.getInstance();
+
+        logger1.log("First log message");
+        logger2.log("Second log message");
+
+        System.out.println("Are both loggers same? " + (logger1 == logger2));
+    }
+}
